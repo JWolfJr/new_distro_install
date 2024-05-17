6d3bc5dde2b0906b366d30217360b066f08af462
Jack Wolf <mystinker08@gmail.com>
2024-05-17 06:30:24 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+New distro installer is a bash script to help making a fresh Linux install easier.
+This is a work in progress!
---
+++ fresh_distro_intaller.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script will update package manager then install xdg if not already installed.
+# Then open the default web browser and open your preferred tabs
+echo "Updating system and installing xdg"
+
+sudo apt update && sudo apt install xdg-utils
+
+echo "Setting default web brownser"
+
+sudo xdg-settings set default-web-browser google-chrome.desktop
+
+echo "Opening default browser and requested tabs"
+
+sudo xdg-open https://kdenlive.org/
+sudo xdg-open https://github.com/SoftFever/OrcaSlicer/
+sudo xdg-open https://inkscape.org/
+sudo xdg-open https://github.com/EdgeTX/edgetx/
+sudo xdg-open https://www.gimp.org/
+sudo xdg-open https://code.visualstudio.com/
+sudo xdg-open https://www.arduino.cc/en/software
+
